6e4aa57d53af50a9d7c651bc16f0c7213fcb1a28
Bill Tarr <betarr13@gmail.com>
2020-12-02 00:07:12 -0500

    day 1 done

--- src/advent_of_code.py
+++ src/advent_of_code.py
@@ -1,12 +1,24 @@
 # Day 1
 
-def get2020(report):
+# Part 1
+def get2020SumOfTwo(report):
     for i in range(0, len(report)):
         for j in range(0, len(report)):
             if i == j:
                 continue
             if report[i] + report[j] == 2020:
                 return report[i] * report[j]
+
+
+# Part 2
+def get2020SumOfThree(report):
+    for i in range(0, len(report)):
+        for j in range(0, len(report)):
+            for k in range(0, len(report)):
+                if i == j or j == k or i == k:
+                    continue
+                if report[i] + report[j] + report[k] == 2020:
+                    return report[i] * report[j] * report[k]
 
 
 def readReportFromFile(path):
@@ -18,7 +30,7 @@ def readReportFromFile(path):
 
 def main():
     expense_report = readReportFromFile('./input/day_1_part_1.txt')
-    return get2020(expense_report)
+    return get2020SumOfThree(expense_report)
 
 
 print(main())
