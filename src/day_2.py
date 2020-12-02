d3ab19227034a47883dd7f165571880b28317606
Bill Tarr <betarr13@gmail.com>
2020-12-02 00:23:52 -0500

    day 2 part 2

--- src/day_2.py
+++ src/day_2.py
@@ -14,11 +14,16 @@ def getIsPasswordValidPart1(password_entry):
     return (char_count >= min_char_count and char_count <= max_char_count)
 
 
+def getIsPasswordValidPart2(password_entry):
+    (first_char_position, second_char_position, char, password) = password_entry
+    return (password[first_char_position - 1] == char and password[second_char_position - 1] != char) or (password[first_char_position - 1] != char and password[second_char_position - 1] == char)
+
+
 def main():
     password_entries = parsePasswordDb('./input/day_2_input.txt')
     valid_passwords = []
     for password_entry in password_entries:
-        if getIsPasswordValid(password_entry):
+        if getIsPasswordValidPart2(password_entry):
             valid_passwords.append(password_entry)
     return len(valid_passwords)
 
